a5ef82288bd447218d23ae82d84a7e36f1bf5731
Lucas Takamoto <lucas.takamoto@hotmail.com>
2023-07-04 13:07:34 -0300

    Exercicios

---
+++ aula-040723/exercicios.js
@@ -0,0 +1,50 @@
+// // Exercício par ou ímpar
+// let num = 10;
+// let condicao = (num % 2 ==0);
+// let mensagem = condicao ? "Par" : "Impar"
+// console.log(mensagem);
+
+// Média arimética de três números
+// const num1 = 5;
+// const num2 = 6;
+// const num3 = 10;
+
+// let media;
+// media = num1 + num2 + num3 / 3;
+// console.log(media);
+
+// Divisivel por 3 e por 5 ao mesmo tempo
+// let n = 9;
+// let dvs = (((n % 3) && (n % 5)) == 0);
+// console.log(dvs);
+
+
+// Calculo do triângulo
+// const base = 16;
+// const altura = 20;
+// result = base * altura / 2;
+// console.log(result);
+
+
+// Temperatura de graus Celsius para Fahrenheit
+// let celsius = 4;
+// let fahr = ((celsius * 9 / 5) + 32);
+// console.log(fahr);
+
+
+
+// Calculo do delta das funções de segundo grau
+// let n1 = 8;
+// let n2 = 2;
+// let n3 = 6;
+// let delta = ((n2 ** 2) - (4 * n1 * n3));
+// console.log(delta);
+
+
+// Pessoa pode dirigir com base na idade
+// let mensagem;
+// let idade = 15;
+// let condicao = (idade >= 18) && (idade <= 80);
+
+// mensagem = condicao ? "Pode" : "Não pode";
+// console.log(mensagem);
