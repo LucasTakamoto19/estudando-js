40248611a57ce90cffde4a4585471664f9c2a554
Lucas Takamoto <lucas.takamoto@hotmail.com>
2023-07-05 09:50:36 -0300

    Concatenação

---
+++ aula-050723/variaveis-concatenacao.js
@@ -0,0 +1,21 @@
+// Escopo Global
+
+// let numero1 = 22;
+// console.log(numero1);
+
+// console.log(numero2);
+// var numero2 = 24; 
+
+// Operador de Concatenação +
+// const numero = 2 + 3; // Operador de Adição 
+// const texto = "O número é " + numero; // Operador de concatenação
+// console.log(texto);
+
+// console.log("A soma de " + 2 + " com " + 3 + " é igual a " + numero);
+
+let idade = 18;
+let mensagem = (idade < 18) ? "Sua idade é " + idade + " pode ser preso! " : " Sua idade é " + idade + " vai ser preso";
+// console.log(mensagem);
+
+const nome = "Lucas";
+console.log("Bem-vindo, ", nome);
