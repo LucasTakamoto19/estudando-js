36601760eff9eb3e9c0a4fdcff082d1393a86115
Lucas Takamoto <lucas.takamoto@hotmail.com>
2023-07-07 09:05:35 -0300

    Repetições

---
+++ aula-060723/repeticoes.js
@@ -0,0 +1,52 @@
+// Estrutura de Repetições
+
+// for => repetições determinadas, ou seja, quantas você quiser
+// while => repetições indeterminadas
+// i => interação
+// 
+// console.log("==================")
+// for(let numero = 0; numero < 10; numero++){
+//     console.log(numero);
+// }
+
+// // Incrementa o valor de número em duas unidades
+// console.log("==================")
+// for(let numero = 0; numero < 10; numero+= 2){
+//     console.log(numero);
+// }
+
+// // Contagem regressiva
+// console.log("==================")
+// for(let numero = 0; numero < 10; numero--){
+//     console.log(numero);
+// }
+
+// for(atribuição; condição; reatribuição){
+//     código de repetição
+// }
+
+// for(let numero = 0; numero < 100; numero++){
+//     console.log(numero);
+// }
+
+// while => enquanto
+// loop infinito
+// let n = 0;
+// while (n < 17) {
+//     console.log("SoulCode + Porto");
+// }
+
+let num = 693;
+let ePrimo = true;
+for(let i = 2; i < num; i++){
+    if (num % i == 0) {
+        ePrimo = false;
+    }
+}
+
+if (ePrimo) {
+    console.log("É primo");
+}
+else {
+    console.log("Não é primo!");
+}
