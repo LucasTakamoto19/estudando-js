26830d6333ef684854613d2b17e097aae5e31550
Lucas Takamoto <lucas.takamoto@hotmail.com>
2023-07-07 10:20:11 -0300

    Repetições

---
+++ aula-070723/repeticoes.js
@@ -0,0 +1,47 @@
+/*
+// Uma quantidade determinada de repetições
+for(inicializacao; condicao; incrementacao){
+    // repeticao...
+}
+
+// Uma quantidade indeterminada de repetições
+while(){
+
+}
+*/
+
+
+// Desenhe um quadradro de # na tela
+let lado =  5;
+let linha = "";
+// for(let i = 0; i < 4; i++){
+//     let linha = ""
+//     for (let j = 0; j < lado; j++) {
+//         linha += "# ";
+//     }
+//     console.log(linha);
+// }
+
+// for (let i = 0; i < lado; i++) {
+//     linha += "# ";
+// }
+// for (let i = 0; i < lado; i++) {
+//     console.log(linha);
+// }
+
+// Break e continue
+// Ignoraa todos os impares
+// for(let i = 1; i <= 20; i++){
+//     if(!(i % 2 == 0)){
+//         continue; // Pula o loop
+//     }
+//     console.log(i)
+// }
+
+// Imprime o primeiro numero multiplo de 8
+for(let i = 1; i <= 100; i++){
+    if(i % 8 == 0 && (i != 8)){
+        console.log(i);
+        break;
+    }
+}
